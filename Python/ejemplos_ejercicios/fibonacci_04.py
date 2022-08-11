dfe741f2c434579b1642026c27c0bae1ac74eb1d
Rodolfo Ugalde <rodolfougaldeochoa@gmail.com>
2022-08-10 21:34:13 -0500

    :construction: python files fibonacci

---
+++ Python/ejemplos_ejercicios/fibonacci_04.py
@@ -0,0 +1,23 @@
+# for python 3.8.10
+from functools import lru_cache
+
+@lru_cache(maxsize=10000)
+def fibonacci(n):
+    """Si el valor se encuentra en cache se retorna.
+    """
+    if type(n) != int:
+        raise TypeError("n debe de ser entero")
+    if type(n) < 1:
+        raise ValueError("n debe de ser positivo")
+
+    if n == 1:
+        return 1
+    elif n == 2:
+        return  1
+    elif n > 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+
+for n in range(1, 51):
+    print(f"{n} : {fibonacci(n)}")
+
